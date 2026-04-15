f7eb2952faf20813959fa75dea557cb71e729aa9
miguelroman1 <23308060610314@cetis61.edu.mx>
2026-04-14 20:44:00 -0600

    hol

--- src/main.py
+++ src/main.py
@@ -1,2 +1,5 @@
 import flet as ft
-
+from controllers.TareaControllers import TareaController
+from controllers.UserController import UserController
+from views.LoginView import LoginView
+from views.dashboard import DashboardView
